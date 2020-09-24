993cfa4131a0b8e3292b1a5090657afbe4394fb2
FedePM <fedepm2002@gmail.com>
2020-09-24 17:51:49 -0300

    Trabajo Practico 1 v1.2

--- Combate.cs
+++ Combate.cs
@@ -1,10 +1,8 @@
-using System;
 public class Combate{
     
     #region Atributos
     private Superheroe Superheroe;
     private Villano Villano;
-    private Superheroe Ganador;
     private int CantidadDeUsosSuperpoder;
     #endregion
 
@@ -13,14 +11,12 @@ public class Combate{
     public Combate(){
         this.Superheroe = new Superheroe();
         this.Villano = new Villano();
-        this.Ganador = new Superheroe();
         this.CantidadDeUsosSuperpoder = 0;
     }
     // constructor 1
-    public Combate(Superheroe superheroe, Villano villano, Superheroe ganador, int cantidadusos){
+    public Combate(Superheroe superheroe, Villano villano, int cantidadusos){
         this.Superheroe = superheroe;
         this.Villano = villano;
-        this.Ganador = superheroe;
         this.CantidadDeUsosSuperpoder = cantidadusos;
     }
     #endregion
@@ -33,9 +29,6 @@ public class Combate{
     public void SetVillano(Villano villano){
         this.Villano = villano;
     }
-    public void SetGanador(Superheroe ganador){
-        this.Ganador = ganador;
-    }
     public void SetCantidadDeUsosSuperpoder(int cantidadusos){
         this.CantidadDeUsosSuperpoder = cantidadusos;
     }
@@ -46,9 +39,6 @@ public class Combate{
     }
     public Villano GetVillano(){
         return this.Villano;
-    }
-    public Superheroe GetGanador(){
-        return this.Ganador;
     }
     public int GetCantidadDeUsosSuperpoder(){
         return this.CantidadDeUsosSuperpoder;
